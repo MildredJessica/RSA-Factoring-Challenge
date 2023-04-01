238aa68bb5e17268204a47dbaa558b6e3813d963
MildredJessica <nwonyejessica@gmail.com>
2023-04-01 21:25:36 +0100

    Factorise numbers

--- factor_func.py
+++ factor_func.py
@@ -1,5 +1,7 @@
 #!usr/bin/python3
-
+import sys
+from resource import getrusage as resource_usage, RUSAGE_SELF
+from time import time as timestamp
 
 def product(n):
     """Factorize as many numbers as possible
@@ -12,3 +14,34 @@ def product(n):
         if ((n % num) == 0):
             print("{}={}*{}".format(n, int(n/num), num))
             return
+
+
+def unix_time(function):
+    '''Return `real`, `sys` and `user` elapsed time, like UNIX's command `time`
+    You can calculate the amount of used CPU-time used by your
+    function/callable by summing `user` and `sys`. `real` is just like the wall
+    clock.
+    Note that `sys` and `user`'s resolutions are limited by the resolution of
+    the operating system's software clock (check `man 7 time` for more
+    details).
+    '''
+    start_time, start_resources = timestamp(), resource_usage(RUSAGE_SELF)
+    function()
+    end_resources, end_time = resource_usage(RUSAGE_SELF), timestamp()
+
+    return "\nreal: {}\nuser: {}\nsys: {}".format(
+        end_time - start_time,
+        end_resources.ru_utime - start_resources.ru_utime,
+        end_resources.ru_stime - start_resources.ru_stime)
+
+
+def factors():
+    """Reads a file to print out all the factors of a number"""
+    with open(sys.argv[1], 'r') as prime:
+        print("Open")
+        line = prime.readline()
+        while line != '':
+            n = int(line)
+            product(n)
+            line = prime.readline()
+
